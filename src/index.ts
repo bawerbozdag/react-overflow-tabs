6e93c42f18626a61e4706b1c8d7890f13f9cada0
bawerbozdag <bawerbozdag@gmail.com>
2025-08-22 21:16:54 +0300

    fix: correct export syntax for `useOverflowTabs` in `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export type { IOverflowState, IOverflowTabsOptions, TOverflowDirection } from "./types";
 
-export * as useOverflowTabs from "./useOverflowTabs";
+export { default as useOverflowTabs } from "./useOverflowTabs";
